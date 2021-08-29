33937e3234e273572a911d3ec930c78b0511ddd3
Janahan Dhushenthen <jdhushenthen@rogers.com>
2021-08-29 10:26:34 -0400

    Embedded demo video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player'
+import demo from './demo.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h3> Elevator Simulation </h3>
+      </div>
+      <div className="Info">
+        <p> Created by: Janahan Dhushenthen and Brandon Bwanakocha </p>
+        <p> Last updated: 13/11/2020 </p>
+      </div>
+      <div className="Video">
+        <ReactPlayer url={demo} width="100%" height="100%" controls={true} />
+      </div>
     </div>
   );
 }
